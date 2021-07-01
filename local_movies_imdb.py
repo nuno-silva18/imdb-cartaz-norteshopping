4d2e7eef4e48d5baa030a8e087fae0f98b951f35
nuno-silva18 <nuno_neto_silva18@hotmail.com>
2021-07-01 11:17:00 +0100

    im an idiot that has learned nothing about scope in 3 years

--- local_movies_imdb.py
+++ local_movies_imdb.py
@@ -24,13 +24,12 @@ for a_link in cartaz_norteshop_a_links:
 imdb_obj = IMDb()
 
 for movie in movies_names_list:
-    print(movie)
     for stop_word in stop_words_movies:
         if stop_word in movie:
-            movie_temp = movie.replace(stop_word, '').strip() # Remove words that might throw off search on IMDB website
-            movie_temp = re.sub('[($-)]+', '', movie_temp) # Remove parentheses with no chars between them from the titles of the movie
-    print(movie_temp)
-    movie_search_results = imdb_obj.search_movie(movie_temp)
+            movie = movie.replace(stop_word, '').strip() # Remove words that might throw off search on IMDB website
+            movie = re.sub('[($-)]+', '', movie) # Remove parentheses with no chars between them from the titles of the movie
+    print(movie)
+    movie_search_results = imdb_obj.search_movie(movie)
     if movie_search_results:
         movie_obj = movie_search_results[0]
         movie_obj_id = movie_obj.getID()
